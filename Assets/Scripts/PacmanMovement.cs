a5fc132557cbf15a108e9b470a153dc123ad9ada
weijiewang001 <65530009+weijiewang001@users.noreply.github.com>
2021-09-23 23:40:06 +1000

    before draw map

--- Assets/Scripts/PacmanMovement.cs
+++ Assets/Scripts/PacmanMovement.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PacmanMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animatorController;
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            animatorController.SetTrigger("PacmanLeft");
+        }
     }
+
 }
