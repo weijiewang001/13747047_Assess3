16d6c67c4755e6f09136bd665d74178bba7231e3
weijiewang001 <65530009+weijiewang001@users.noreply.github.com>
2021-09-25 21:16:34 +1000

    90

--- Assets/Scripts/PacmanMovement.cs
+++ Assets/Scripts/PacmanMovement.cs
@@ -6,20 +6,53 @@ using UnityEngine;
 
 public class PacmanMovement : MonoBehaviour
 {
+    private bool switchedOn = false;
+    private GameObject pacman = default;
+    private Animator pacmanAnimator = default;
     public Animator animatorController;
     public AudioSource moveAudio;
+    float speed = 2.5f;
+
     private void Start()
     {
-        
+        pacman = GameObject.FindWithTag("Pacman"); 
+        pacman.transform.position = new Vector3(-12.5f, 13.5f, 0f);
+        //pacmanAnimator = pacman.GetComponent<Animator>();
+        pacmanAnimator.SetTrigger("Pacman_TurnRight");
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        if (pacman.transform.position.x >= -12.5f && pacman.transform.position.x < -1.5f && pacman.transform.position.y == 13.5f)
         {
-            animatorController.SetTrigger("PacmanLeft");
-            moveAudio.Play();
+            pacman.transform.position += (Vector3.right * speed * Time.deltaTime);
         }
+        if (pacman.transform.position.x == -1.5f && pacman.transform.position.y <= 13.5f && pacman.transform.position.y > 9.5)
+        {
+            pacman.transform.position += (Vector3.down * speed * Time.deltaTime);
+        }
+        if (pacman.transform.position.x != -1.5f && pacman.transform.position.y == 13.5f)
+        {
+            pacman.transform.position += (Vector3.right * speed * Time.deltaTime);
+        }
+        else if (pacman.transform.position.x == -1.5f && pacman.transform.position.y != 9.5)
+        {
+            pacman.transform.position += (Vector3.down * speed * Time.deltaTime);
+        }
+        else if (pacman.transform.position.x != -12.5f && pacman.transform.position.y == 9.5)
+        {
+            pacman.transform.position += (Vector3.left * speed * Time.deltaTime);
+        }
+        else
+        {
+            pacman.transform.position += (Vector3.up * speed * Time.deltaTime);
+        }
+
+        //if(Input.GetKeyDown(KeyCode.A))
+        //{
+        //    animatorController.SetTrigger("PacmanLeft");
+        //    moveAudio.Play();
+        //}
     }
 
 }
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Tweener : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Tween activeTween;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (activeTween != null && activeTween.Target != null)
+        {
+            if (Vector3.Distance(activeTween.Target.position, activeTween.EndPos) > 0.1f)
+            {
+                float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration;
+                activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
+                //activeTween.Target.position = activeTween.StartPos + activeTween.EndPos * timeFraction * timeFraction * timeFraction;
+            }
+            else
+            {
+                activeTween.Target.position = activeTween.EndPos;
+                activeTween = null;
+            }
+        }
+    }
+    public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+
+        if (activeTween == null)
+        {
+            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+        }
+        else
+        {
+            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+        }
     }
 }
