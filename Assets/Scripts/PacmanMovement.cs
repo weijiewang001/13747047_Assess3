9e5e86d8ed2ffa0d6146496c04e6812d44756553
weijiewang001 <65530009+weijiewang001@users.noreply.github.com>
2021-09-25 22:55:57 +1000

    90% completed

--- Assets/Scripts/PacmanMovement.cs
+++ Assets/Scripts/PacmanMovement.cs
@@ -6,46 +6,52 @@ using UnityEngine;
 
 public class PacmanMovement : MonoBehaviour
 {
-    private bool switchedOn = false;
+    //private bool switchedOn = false;
     private GameObject pacman = default;
     private Animator pacmanAnimator = default;
-    public Animator animatorController;
+    //public Animator animatorController;
     public AudioSource moveAudio;
     float speed = 2.5f;
+    float timeElapsed;
+    const float lerpDurationA = 4.8f;
+    const float lerpDurationB = 2f;
+
 
     private void Start()
     {
         pacman = GameObject.FindWithTag("Pacman"); 
         pacman.transform.position = new Vector3(-12.5f, 13.5f, 0f);
-        //pacmanAnimator = pacman.GetComponent<Animator>();
-        pacmanAnimator.SetTrigger("Pacman_TurnRight");
+        pacmanAnimator = pacman.GetComponent<Animator>();
+        //pacmanAnimator.SetTrigger("Pacman_TurnRight");
+        timeElapsed = 0;
+
     }
 
     private void Update()
     {
-        if (pacman.transform.position.x >= -12.5f && pacman.transform.position.x < -1.5f && pacman.transform.position.y == 13.5f)
+        if (timeElapsed >= lerpDurationA && timeElapsed < (lerpDurationA + lerpDurationB))
         {
-            pacman.transform.position += (Vector3.right * speed * Time.deltaTime);
+            pacman.transform.position = Vector3.Lerp(new Vector3(-1.5f, 13.5f, 0), new Vector3(-1.5f, 9.5f, 0), (timeElapsed - lerpDurationA) / lerpDurationB);
+            timeElapsed += Time.deltaTime;
         }
-        if (pacman.transform.position.x == -1.5f && pacman.transform.position.y <= 13.5f && pacman.transform.position.y > 9.5)
+        if (timeElapsed >= (lerpDurationA + lerpDurationB) && timeElapsed < (lerpDurationA * 2 + lerpDurationB))
         {
-            pacman.transform.position += (Vector3.down * speed * Time.deltaTime);
+            pacman.transform.position = Vector3.Lerp(new Vector3(-1.5f, 9.5f, 0), new Vector3(-12.5f, 9.5f, 0), (timeElapsed - lerpDurationA - lerpDurationB) / lerpDurationA);
+            timeElapsed += Time.deltaTime;
         }
-        if (pacman.transform.position.x != -1.5f && pacman.transform.position.y == 13.5f)
+        if (timeElapsed >= (lerpDurationA * 2 + lerpDurationB) && timeElapsed < 2 * (lerpDurationA + lerpDurationB))
         {
-            pacman.transform.position += (Vector3.right * speed * Time.deltaTime);
+            pacman.transform.position = Vector3.Lerp(new Vector3(-12.5f, 9.5f, 0), new Vector3(-12.5f, 13.5f, 0), (timeElapsed - lerpDurationA * 2 - lerpDurationB) / lerpDurationB);
+            timeElapsed += Time.deltaTime;
         }
-        else if (pacman.transform.position.x == -1.5f && pacman.transform.position.y != 9.5)
+        if (timeElapsed >= 0 && timeElapsed < lerpDurationA)
         {
-            pacman.transform.position += (Vector3.down * speed * Time.deltaTime);
+            pacman.transform.position = Vector3.Lerp(new Vector3(-12.5f, 13.5f, 0), new Vector3(-1.5f, 13.5f, 0), timeElapsed / lerpDurationA);
+            timeElapsed += Time.deltaTime;
         }
-        else if (pacman.transform.position.x != -12.5f && pacman.transform.position.y == 9.5)
+        if (timeElapsed >= 2 * (lerpDurationA + lerpDurationB))
         {
-            pacman.transform.position += (Vector3.left * speed * Time.deltaTime);
-        }
-        else
-        {
-            pacman.transform.position += (Vector3.up * speed * Time.deltaTime);
+            timeElapsed = 0;
         }
 
         //if(Input.GetKeyDown(KeyCode.A))
