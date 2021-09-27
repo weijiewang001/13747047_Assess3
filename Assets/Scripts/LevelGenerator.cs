53ae58dfac37e2da9fb17a4e2658787a635648c1
weijiewang001 <65530009+weijiewang001@users.noreply.github.com>
2021-09-27 19:22:28 +1000

    dinner

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -23,35 +23,31 @@ public class LevelGenerator : MonoBehaviour
                         {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
                         {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
                        };
-    public static int Row = 15;
-    public static int Column = 14;
+    public int Row = 15;
+    public int Column = 14;
     public static int x;
     public static int y;
     private int i;
-    private int map1;
-    private bool signal = false;
     private int r;
     private int c;
     private int d;
     private int count;
-    [SerializeField]private GameObject[] MapGO = default;
+    [SerializeField] private GameObject[] MapGO = default;
     private GameObject[] Topleft = new GameObject[211];
-    //private GameObject[] upperRight = new GameObject[211];
-    //private GameObject[] lowerLeft = new GameObject[197];
-    //private GameObject[] lowerRight = new GameObject[197];
+    private static int[] NintydegreePosition = { 15, 28, 29, 42, 43, 45, 48, 50, 54, 56, 57,59,64,70, 71, 85, 99,101, 106,109, 107, 113, 120, 121, 126,127, 134,135, 140, 146, 148,154, 160, 162, 163, 174, 176, 177,190, 193, 207 };
+    private static int[] NegativeNintyPosition = { 34, 40, 90, 93, 112, 132, 138 };
+    private static int[] HundredeightyPosition = { 62, 68, 104, 152, 188, 191 };
 
     private GameObject map;
     private GameObject grid = default;
-    
+
 
     void Awake()
     {
         map = GameObject.FindWithTag("Map");
         grid = GameObject.FindWithTag("Grid");
-        UpperLeft();
-        //UpperRight();
-        //LowerLeft();
-        //LowerRight();
+        TopLeft();
+
 
     }
 
@@ -60,7 +56,7 @@ public class LevelGenerator : MonoBehaviour
         grid.SetActive(false);
     }
 
-    private void UpperLeft()
+    private void TopLeft()
     {
         i = 1;
         x = -14;
@@ -74,152 +70,24 @@ public class LevelGenerator : MonoBehaviour
                 Topleft[i].name = "MapTopLeft" + i;
                 Topleft[i].transform.parent = gameObject.transform;
                 i++;
-
             }
         }
-
-        for (int newrow = 0; newrow < Row; newrow++)
+        foreach (int item in NintydegreePosition)
         {
-            for (int newcolumn = 0; newcolumn < Column; newcolumn++)
-            {
-                if (levelMap[newrow, newcolumn] == 1)
-                {
-
-                    //Topleft[i].transform.rotation = Quaternion.Euler(0f, 0f, 180.0f); 
-                    signal = true;
-                    while (signal)
-                    {
-                        r = newrow + 1;
-                        c = newcolumn;
-                        if (levelMap[r, c] == 2)
-                        {
-                            d = r + c;
-                            Topleft[d].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
-                            break;
-                        }
-                        else
-                        {
-                            signal = false;
-                        }
-                    }
-                    i++;
-
-
-
-
-                    //while (levelMap[r, c] == 2)
-                    //{
-                    //    d = r + c;
-                    //    Topleft[d].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
-                    //}
-
-                    //if (levelMap[r,c] == 2)
-                    //{
-                    //    d = r + c;
-                    //    Topleft[d].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
-                    //}
-
-                    //if (levelMap[r, c] == 2)
-                    //{
-                    //    d = r + c;
-                    //    Topleft[d].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
-                    //}
-                }
-            }
-
+            Topleft[i].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
+        }
+        foreach (int item in NegativeNintyPosition)
+        {
+            Topleft[i].transform.rotation = Quaternion.Euler(0f, 0f, -90.0f);
+        }
+        foreach(int item in HundredeightyPosition)
+        {
+            Topleft[i].transform.rotation = Quaternion.Euler(0f, 0f, 180.0f);
         }
 
-
-                
-
-
-                //i = 1;
-                //foreach (Transform child in transform)
-                //{
-
-                //    if (child.name == "MapTopLeft1")
-                //    {
-
-                //        child.transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
-                //    }
-                //}
-                ////if (levelMap[newrow, newcolumn] == 2)
-                //{
-                //    newrow += 1;
-                //    if (levelMap[newrow, newcolumn] == 2)
-                //    {
-                //        Topleft[newrow + newcolumn].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
-                //        newrow += 1;
-                //    }
-                //}
-
-
-
-
-                //foreach (int i in NintyDegree)
-                //{
-
-
-                //    upperLeft[i].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
-                //}
-                //foreach (int i in NegativeNintyDegree)
-                //{
-                //    upperLeft[i].transform.rotation = Quaternion.Euler(0f, 0f, -90.0f);
-                //}
-                //foreach (int i in HundredEightyDegree)
-                //{
-                //    upperLeft[i].transform.rotation = Quaternion.Euler(0f, 0f, 180.0f);
-                //}
-                //foreach (int i in NegativeNintyDegree)
-                //{
-                //    upperLeft[i].transform.rotation = Quaternion.Euler(0f, 0f, 270.0f);
-                //}
-            }
-    //private void UpperRight()
-    //{
-    //    i = 1;
-    //    x = 13;
-    //    y = 15;
-    //    for (int row = 0; row < Row; row++)
-    //    {
-    //        for (int column = 0; column < Column; column++)
-    //        {
-    //            upperRight[i] = Instantiate(MapGameObjects[levelMap[row, column]], new Vector2(x - column, y - row), Quaternion.identity);
-    //            if (levelMap[row, column] == 5)
-    //            {
-    //                upperRight[i].transform.parent = dot.transform;
-    //            }
-    //            else if (levelMap[row, column] == 6)
-    //            {
-    //                //upperRight[i].transform.parent = PowerPellet.transform;
-    //            }
-    //            else
-    //            {
-    //                upperRight[i].transform.parent = map.transform;
-    //            }
-    //            upperRight[i].name = "MapUpperRight" + i;
-    //            if (i <= 210) i++;
-    //        }
-    //    }
-    //    foreach (int i in RotationLine)
-    //    {
-    //        upperRight[i].transform.Rotate(0f, 0f, -90.0f);
-    //    }
-
-    //    foreach (int i in RotationA)
-    //    {
-    //        upperRight[i].transform.Rotate(0f, 0f, -90.0f);
-    //    }
-    //    foreach (int i in RotationC)
-    //    {
-    //        upperRight[i].transform.Rotate(0f, 0f, 180.0f);
-    //    }
-    //    foreach (int i in RotationD)
-    //    {
-    //        upperRight[i].transform.Rotate(0f, 0f, 90f);
-    //    }
-    //    upperRight[Tjuc].GetComponent<SpriteRenderer>().flipX = true;
-
-
+    }
 
 }
+
+
+
